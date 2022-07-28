c5e06d24c85d71d2cb65b47acc981f054fa6d58d
Erik Yamamoto <erik.ymmt@gmail.com>
2022-07-28 19:10:56 -0300

    add completed expense form - Requirement 3 ✓

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -14,13 +14,38 @@ class WalletForm extends Component {
     const { currencies } = this.props;
     return (
       <form>
-        <input type="number" data-testid="value-input" />
-        <input type="text" data-testid="description-input" />
-        <select name="currency" id="currency" data-testid="currency-input">
-          {currencies.map((currency) => (
-            <option key={ currency } value={ currency }>{currency}</option>)) }
-        </select>
-        <input type="" />
+        <label htmlFor="value">
+          Valor:
+          <input type="number" data-testid="value-input" />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <input type="text" data-testid="description-input" />
+        </label>
+        <label htmlFor="currencies">
+          Moeda:
+          <select name="currency" id="currency" data-testid="currency-input">
+            {currencies.map((currency) => (
+              <option key={ currency } value={ currency }>{currency}</option>)) }
+          </select>
+        </label>
+        <label htmlFor="method">
+          <select data-testid="method-input">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Cartão de crédito</option>
+            <option value="debito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          <select data-testid="tag-input">
+            <option value="alimentacao">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saude">Saúde</option>
+          </select>
+        </label>
+
       </form>
     );
   }
