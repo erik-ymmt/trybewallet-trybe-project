169d723ca08f4b302f51a70aed04bffcf0edd554
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-01 18:48:41 -0300

    header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import '../styles/Header.css';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     const { email, totalExpenses } = this.props;
     return (
       <header>
-        <div data-testid="email-field">
-          {email}
-        </div>
-        <div data-testid="total-field">
-          {totalExpenses()}
-        </div>
-        <div data-testid="header-currency-field">
-          BRL
-        </div>
+        <h3>Trybewallet</h3>
+        <section>
+          <div>
+            <span>
+              Email:
+              {' '}
+            </span>
+            <span data-testid="email-field">
+              {email === ''
+                ? <Link to="/">Faça o Login</Link>
+                : email}
+            </span>
+          </div>
+          <div>
+            <span>
+              Despesas Totais:
+              {' '}
+            </span>
+            <span data-testid="total-field">
+              <strong>{totalExpenses()}</strong>
+            </span>
+            <span data-testid="header-currency-field">
+              {' '}
+              BRL
+            </span>
+          </div>
+        </section>
+
       </header>
     );
   }
@@ -31,9 +52,6 @@ const calculateTotalExpenses = (state) => {
     const originalValue = cur.value;
     const exchangeRate = cur.exchangeRates[exchangeCurrency].ask;
     const exchangedValue = originalValue * exchangeRate;
-    // console.log(exchangeCurrency, 'exchangeCurrency');
-    // console.log(originalValue, 'originalValue');
-    // console.log(exchangeRate, 'exchangeRate');
     return Number(acc) + exchangedValue;
   }), 0.00);
   return sumExpenses.toFixed(2);
